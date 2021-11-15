3a296d44578fb7d1473c08952ce02eaefa73c3e3
xovest <79008917+xovest@users.noreply.github.com>
2021-11-15 15:25:26 +0200

    indexes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Transition, animated } from "react-spring";
+import Component1 from "./components/Component1";
+import Component2 from "./components/Component2";
+import Component3 from "./components/Component3";
 
-ReactDOM.render(
-  <React.StrictMode>
-    
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "./styles.css";
+
+class App extends React.Component {
+  state = {
+    showComponent3: false
+  };
+
+  toggle = e => this.setState({ showComponent3: !this.state.showComponent3 });
+
+  render() {
+    return (
+      <div className="App">
+        <Component1 />
+        <Component2 toggle={this.toggle} />
+        <Transition
+          native
+          items={this.state.showComponent3}
+          from={{ opacity: 0 }}
+          enter={{ opacity: 1 }}
+          leave={{ opacity: 0 }}
+        >
+          {show =>
+            show &&
+            (props => (
+              <animated.div style={props}>
+                <Component3 />
+              </animated.div>
+            ))
+          }
+        </Transition>
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
